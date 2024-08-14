a3cf8cba8ebd16c856fec29a1f614fcf9bcae404
Yahye Abdulle <yabdulle@outlook.com>
2024-08-14 20:07:39 +0100

    add href to contact us and change contact form

--- app/pages/Contact.tsx
+++ app/pages/Contact.tsx
@@ -5,102 +5,55 @@ import React from "react";
 export function ContactSection() {
     return (
         <section className="body-font relative text-gray-400">
-            <div className="container mx-auto px-5 py-24">
-                <div className="mb-12 flex w-full flex-col text-center">
-                    <h1 className="title-font mb-4 text-2xl font-medium text-white sm:text-3xl">Contact Us</h1>
-                    <p className="mx-auto text-base leading-relaxed lg:w-2/3 text-white">
-                        Feel free to reach out to us! Whether you have a question, feedback, or a collaboration proposal, we'd love to hear from you.
-                    </p>
-                </div>
-
-                <div className="mx-auto md:w-2/3 lg:w-1/2">
-                    <div className="-m-2 flex flex-wrap">
-                        <div className="w-1/2 p-2">
-                            <div className="relative">
+            <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+                <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                    <div
+                        className="absolute inset-0 bg-gradient-to-r from-green-700 to-lime-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"
+                    />
+                    <div className="text-white relative px-4 py-10 bg-green-400 shadow-lg sm:rounded-3xl sm:p-20">
+                        <div className="text-center pb-6">
+                            <h1 className="text-3xl">Contact Us!</h1>
+                            <p className="text-white">
+                                Fill up the form below to send us a message.
+                            </p>
+                        </div>
+                        <form>
+                            <input
+                                className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="Name"
+                                name="name"
+                            />
+                            <input
+                                className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                            />
+                            <input
+                                className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="Subject"
+                                name="_subject"
+                            />
+                            <textarea
+                                className="shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                placeholder="Type your message here..."
+                                name="message"
+                                style={{ height: '121px' }}
+                            />
+                            <div className="flex justify-between">
                                 <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    className="peer w-full rounded border border-gray-700 bg-transparent py-1 px-3 text-base leading-8 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-                                    placeholder="Name"
+                                    className="shadow bg-gradient-to-r from-lime-700 to-green-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    type="submit"
+                                    value="Send ➤"
                                 />
-                                <label
-                                    htmlFor="name"
-                                    className="absolute left-3 -top-6 text-sm leading-7 text-indigo-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
-                                >
-                                    Name
-                                </label>
-                            </div>
-                        </div>
-
-                        <div className="w-1/2 p-2">
-                            <div className="relative">
                                 <input
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    className="peer w-full rounded border border-gray-700 bg-transparent py-1 px-3 text-base leading-8 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-                                    placeholder="Email"
+                                    className="shadow bg-gradient-to-r from-green-700 to-lime-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    type="reset"
                                 />
-                                <label
-                                    htmlFor="email"
-                                    className="absolute left-3 -top-6 text-sm leading-7 text-indigo-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
-                                >
-                                    Email
-                                </label>
                             </div>
-                        </div>
-
-                        <div className="mt-4 w-full p-2">
-                            <div className="relative">
-                                <textarea
-                                    id="message"
-                                    name="message"
-                                    className="peer h-32 w-full resize-none rounded border border-gray-700 bg-transparent py-1 px-3 text-base leading-6 text-gray-100 placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-                                    placeholder="Message"
-                                ></textarea>
-                                <label
-                                    htmlFor="message"
-                                    className="absolute left-3 -top-6 text-sm leading-7 text-indigo-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
-                                >
-                                    Message
-                                </label>
-                            </div>
-                        </div>
-
-                        <div className="w-full p-2">
-                            <button className="mx-auto flex rounded border-0 bg-indigo-500 py-2 px-8 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-                                Send
-                            </button>
-                        </div>
-
-                        <div className="mt-8 w-full border-t border-gray-800 p-2 pt-8 text-center">
-                            <a className="text-indigo-400">example@email.com</a>
-                            <p className="my-5 leading-normal">49 Smith St. <br />Saint Cloud, MN 56301</p>
-                            <span className="inline-flex">
-                                <a className="text-gray-500">
-                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="h-5 w-5" viewBox="0 0 24 24">
-                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                    </svg>
-                                </a>
-                                <a className="ml-4 text-gray-500">
-                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="h-5 w-5" viewBox="0 0 24 24">
-                                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                                    </svg>
-                                </a>
-                                <a className="ml-4 text-gray-500">
-                                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="h-5 w-5" viewBox="0 0 24 24">
-                                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                    </svg>
-                                </a>
-                                <a className="ml-4 text-gray-500">
-                                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="h-5 w-5" viewBox="0 0 24 24">
-                                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                    </svg>
-                                </a>
-                            </span>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
