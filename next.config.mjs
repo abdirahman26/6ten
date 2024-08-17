4e3ae498cd7d34bf4c268545f5aa2e3dca7e992e
Yahye Abdulle <yabdulle@outlook.com>
2024-08-17 20:26:06 +0100

    small fixes done

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.ctfassets.net'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.ctfassets.net',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.britannica.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 };
 
