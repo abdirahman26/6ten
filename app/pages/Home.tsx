33a0a1bc5a5f0995baad142c15ad0c72c608c3e4
Yahye Abdulle <yabdulle@outlook.com>
2024-08-17 18:10:11 +0100

    fix, using config now

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -4,6 +4,7 @@ import { m, motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
 import { TypeWriterEffect } from "@components/TypeWriterEffect";
+import { InfiniteSlider } from "@components/InfiniteSlider";
 
 function Home() {
   const slideVariants = [
@@ -40,51 +41,8 @@ function Home() {
           <span>Join now →</span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-[var(--linePink)] to-transparent" />
         </button>
-        <section className="text-white py-8 w-[40%]">
-          <h2 className="text-center text-2xl mb-2 font-bold leading-8">Our Clients</h2>
-          <p className="text-center text-lg font-extralight leading-8">We are trusted by the world’s most innovative teams</p>
 
-          <div className="logos group relative overflow-hidden whitespace-nowrap py-10">
-            <div className="carousel flex animate-slide-left-infinite">
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg" alt="Transistor" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg" alt="Reform" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg" alt="Tuple" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg" alt="SavvyCal" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg" alt="Statamic" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/laravel-logo-white.svg" alt="Laravel" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg" alt="Transistor" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg" alt="Reform" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg" alt="Tuple" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg" alt="SavvyCal" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg" alt="Statamic" />
-              </div>
-              <div className="flex-shrink-0 mx-4 h-16">
-                <img src="https://tailwindui.com/img/logos/158x48/laravel-logo-white.svg" alt="Laravel" />
-              </div>
-            </div>
-          </div>
-        </section>
+          <InfiniteSlider />
 
       </div>
     </>
